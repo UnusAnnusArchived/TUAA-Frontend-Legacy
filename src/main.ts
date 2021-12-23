54887299dff083a314c22d231c3606a655a721bd
ScribbleNerd <contact@scribblenerd.com>
2021-12-23 01:55:55 -0600

    Mostly finish watch page + add episode number on home page

--- src/main.ts
+++ src/main.ts
@@ -17,4 +17,57 @@ app.get("/", async(req, res) => {
   });
 });
 
+app.get("/watch/:video", async(req, res) => {
+  const watchCode = req.params.video;
+  const episode = await tuaa.v2.metadata.episode(watchCode);
+  let resolution: string;
+  let videoUrl: string;
+
+  if (episode.sources) {
+    var videoHighestRes: string;
+
+    let lastHighestRes = 0;
+    let lastHighestResIndex: number;
+    for (let i = 0; i < episode.sources.length; i++) {
+      if (episode.sources[i].size > lastHighestRes) {
+        lastHighestRes = episode.sources[i].size;
+        lastHighestResIndex = i;
+      }
+    }
+  
+    videoHighestRes = lastHighestRes.toString();
+  
+    resolution = <string>req.query.res ?? videoHighestRes;
+  
+    let resolutionExists = false;
+    for (var i = 0; i < episode.sources.length; i++) {
+      if (resolution === episode.sources[i].size.toString()) {
+        resolutionExists = true;
+        break;
+      }
+    }
+  
+    if (!resolutionExists) {
+      resolution = videoHighestRes;
+    }
+
+    //Find video url for wanted resolution
+    for (var i = 0; i < episode.sources.length; i++) {
+      if (resolution === episode.sources[i].size.toString()) {
+        videoUrl = episode.sources[i].src;
+        break;
+      }
+    }
+  } else {
+    resolution = "noresolutions";
+    videoUrl = episode.video;
+  }
+
+  res.render("watch", {
+    episode,
+    resolution,
+    videoUrl
+  });
+});
+
 app.listen(4269);
