fcc63195597ac1b6c4cdcbb35a627b7519147222
ScribbleNerd <contact@scribblenerd.com>
2021-12-23 00:31:32 -0600

    Added season navigation
    Currently axios will throw a 404 error because tuaa-api needs to be updated

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
+import express from "express";
+import TUAA from "tuaa-api";
 
+const app = express();
+const tuaa = new TUAA();
+
+app.set("view engine", "ejs");
+
+app.get("/", async(req, res) => {
+  const seasons = await tuaa.v2.metadata.all();
+  const currentSeasonIndex = parseInt(req.query.season?.toString?.() || "1");
+
+  res.render("home", {
+    seasonCount: seasons.length,
+    currentSeasonIndex,
+    episodes: seasons[currentSeasonIndex]
+  });
+});
+
+app.listen(4269);
