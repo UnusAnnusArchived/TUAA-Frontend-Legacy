d16f3d0014b792dcc20fb8d69d2de490b8f508e5
ScribbleNerd <contact@scribblenerd.com>
2022-04-24 09:10:45 -0500

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 import express from "express";
-import TUAA from "tuaa-api";
+import TUAA, { LimitedUser } from "tuaa-api";
 import { resolve } from "path";
+import moment from "moment";
 
 const app = express();
 const tuaa = new TUAA();
 
 app.set("view engine", "ejs");
 
-app.get("/", async(req, res) => {
+app.get("/", async (req, res) => {
   const seasons = await tuaa.v2.metadata.all();
   const currentSeasonIndex = parseInt(req.query.season?.toString?.() || "1");
 
   res.render("home", {
     seasonCount: seasons.length,
     currentSeasonIndex,
-    episodes: seasons[currentSeasonIndex]
+    episodes: seasons[currentSeasonIndex],
+    moment,
   });
 });
 
-app.get("/watch/:video", async(req, res) => {
+app.get("/watch/:video", async (req, res) => {
+  const userInfo: IUserInfo = JSON.parse(req.cookies?.user ?? "{}");
+
   const watchCode = req.params.video;
   const episode = await tuaa.v2.metadata.episode(watchCode);
   const comments = await tuaa.v2.comments.get(watchCode, 0, 1000);
@@ -36,11 +40,11 @@ app.get("/watch/:video", async(req, res) => {
         lastHighestResIndex = i;
       }
     }
-  
+
     videoHighestRes = lastHighestRes.toString();
-  
+
     resolution = <string>req.query.res ?? videoHighestRes;
-  
+
     let resolutionExists = false;
     for (var i = 0; i < episode.sources.length; i++) {
       if (resolution === episode.sources[i].size.toString()) {
@@ -48,7 +52,7 @@ app.get("/watch/:video", async(req, res) => {
         break;
       }
     }
-  
+
     if (!resolutionExists) {
       resolution = videoHighestRes;
     }
@@ -70,7 +74,8 @@ app.get("/watch/:video", async(req, res) => {
     resolution,
     videoUrl,
     comments,
-    isLoggedIn: false
+    userInfo,
+    moment,
   });
 });
 
@@ -78,4 +83,11 @@ app.get("*", (req, res) => {
   res.sendFile(resolve(`${__dirname}/../errors/404.html`));
 });
 
-app.listen(4269);
+app.listen(4269, () => {
+  console.log("Started");
+});
+
+interface IUserInfo {
+  loginKey: string;
+  user: LimitedUser;
+}
